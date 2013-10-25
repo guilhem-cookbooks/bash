88d6170c045ab3349d936f4285a920d5bb0a3642
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-10-25 15:07:32 +0200

    Increment version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'guilhem.lettron@optiflows.com'
 license          'Apache 2.0'
 description      'Installs/Configures bash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
+version          '0.1.1'
