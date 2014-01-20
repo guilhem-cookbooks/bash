4e898d040b07e33edae3355de89b414ac7ca58fa
Lloyd Philbrook <lloyd@tablexi.com>
2014-01-20 23:50:33 +0800

    added delete action spec

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -5,55 +5,67 @@ EtcPwnam = Struct.new(:name, :gid)
 describe 'test::profile' do
   let(:chef_run) do
     pw_user = EtcPwnam.new
-    pw_user.name = 'test1'
-    pw_user.gid = 'test1'
-    Dir.stub(:home).and_return('/home/test1')
+    pw_user.name = 'test_user'
+    pw_user.gid = 'users'
+    Dir.stub(:home).and_return('/home/test_user')
     Etc.stub(:getpwnam).and_return(pw_user)
     ChefSpec::Runner.new(step_into: ['bash_profile'])
   end
 
-  context 'init' do
-    context 'default' do
-      before do
-        File.stub(:exists?).and_call_original
-        IO.stub(:read).and_call_original
-        File.should_receive(:exists?).with('/home/test1/.bash_profile').and_return(true)
-        File.should_receive(:exists?).with('/home/test1/.bash_profile.d/init').and_return(false)
-        IO.should_receive(:read).with('/home/test1/.bash_profile').and_return('')
-        chef_run.converge described_recipe
+  context 'action create' do
+    context 'init' do
+      context 'default' do
+        before do
+          File.stub(:exists?).and_call_original
+          IO.stub(:read).and_call_original
+          File.should_receive(:exists?).with('/home/test_user/.bash_profile').and_return(true)
+          File.should_receive(:exists?).with('/home/test_user/.bash_profile.d/init').and_return(false)
+          IO.should_receive(:read).with('/home/test_user/.bash_profile').and_return('')
+          chef_run.converge described_recipe
+        end
+
+        it 'to create .bash_profile.d directory' do
+          expect(chef_run).to create_directory('/home/test_user/.bash_profile.d')
+        end
+
+        it 'to create .bash_profile.d/init file' do
+          expect(chef_run).to create_file('/home/test_user/.bash_profile.d/init')
+        end
+
+        it 'to replace .bash_profile with cookbook file' do
+          expect(chef_run).to create_cookbook_file('/home/test_user/.bash_profile')
+        end
       end
 
-      it 'to create .bash_profile.d directory' do
-        expect(chef_run).to create_directory('/home/test1/.bash_profile.d')
-      end
+      context 'to touch' do
+        before do
+          chef_run.converge described_recipe
+        end
 
-      it 'to create .bash_profile.d/init file' do
-        expect(chef_run).to create_file('/home/test1/.bash_profile.d/init')
-      end
-
-      it 'to replace .bash_profile with cookbook file' do
-        expect(chef_run).to create_cookbook_file('/home/test1/.bash_profile')
+        it '.bash_profile.d/init file' do
+          expect(chef_run).to touch_file('/home/test_user/.bash_profile.d/init')
+        end
       end
     end
 
-    context 'to touch' do
+    context 'test1' do
       before do
         chef_run.converge described_recipe
       end
 
-      it '.bash_profile.d/init file' do
-        expect(chef_run).to touch_file('/home/test1/.bash_profile.d/init')
+      it 'create .bash_profile.d/test1 file' do
+        expect(chef_run).to create_file('/home/test_user/.bash_profile.d/test1')
       end
     end
   end
 
-  context 'test1' do
+  context 'action remove' do
     before do
       chef_run.converge described_recipe
     end
 
-    it 'create .bash_profile.d/test1 file' do
-      expect(chef_run).to create_file('/home/test1/.bash_profile.d/test1')
+    it 'delete .bash_profile.d/test2 file' do
+      expect(chef_run).to delete_file('/home/test_user/.bash_profile.d/test2')
     end
   end
 
@@ -64,6 +76,10 @@ describe 'test::profile' do
 
     it 'add_bash_profile' do
       expect(chef_run).to add_bash_profile('test1')
+    end
+
+    it 'remove_bash_profile' do
+      expect(chef_run).to remove_bash_profile('test2')
     end
   end
 end
--- spec/support/cookbooks/test/recipes/profile.rb
+++ spec/support/cookbooks/test/recipes/profile.rb
@@ -1,4 +1,9 @@
 bash_profile 'test1' do
-  user 'test1'
+  user 'test_user'
   content 'test1'
+end
+
+bash_profile 'test2' do
+  action :remove
+  user 'test_user'
 end
